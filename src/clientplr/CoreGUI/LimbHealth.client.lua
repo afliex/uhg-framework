22272831fc1231ab8bd7db352d790b6c91e457a0
n6ux <kelechalt@gmail.com>
2024-07-24 15:25:22 +0100

    Made limb health colours more accurate.

--- src/clientplr/CoreGUI/LimbHealth.client.lua
+++ src/clientplr/CoreGUI/LimbHealth.client.lua
@@ -10,7 +10,27 @@ local character = player.Character or player.CharacterAdded:Wait()
 local coreinfo = playergui:WaitForChild("CoreGame")
 local limbhealth = coreinfo.LimbHealth
 
---Limb CoreGUI
+local sequence = ColorSequence.new{
+	ColorSequenceKeypoint.new(0, Color3.fromRGB(255, 73, 76)),
+	ColorSequenceKeypoint.new(0.5, Color3.fromRGB(253, 175, 130)),
+	ColorSequenceKeypoint.new(1, Color3.fromRGB(140, 255, 114))
+}
+
+function returnsequencepoint(x)
+	if x == 0 then return sequence.Keypoints[1].Value end
+	if x == 1 then return sequence.Keypoints[#sequence.Keypoints].Value end
+
+	for i = 1, #sequence.Keypoints - 1 do
+		local current = sequence.Keypoints[i]
+		local next = sequence.Keypoints[i + 1]
+		
+		if x >= current.Time and x < next.Time then
+			local alpha = (x - current.Time) / (next.Time - current.Time)
+			return current.Value:Lerp(next.Value, alpha)
+		end
+	end
+end
+
 for _,v in pairs(character:GetChildren()) do
 	if v:IsA("BasePart") and v.Name ~= "HumanoidRootPart" then
 		v:GetAttributeChangedSignal("Health"):Connect(function()
@@ -20,7 +40,10 @@ for _,v in pairs(character:GetChildren()) do
 			local limbframe = limbhealth:FindFirstChild(v.Name) :: Frame
 			
 			if limbframe then
-				limbframe.BackgroundColor3 = Color3.new(1, 0.447059, 0.447059):Lerp(Color3.new(0.54902, 1, 0.564706), health / maxhealth)
+				print("ye")
+				local sequencepoint = returnsequencepoint(math.clamp(health/maxhealth, 0, 1)) 
+				print(sequencepoint)
+				limbframe.BackgroundColor3 = sequencepoint
 			end
 		end)
 	end
